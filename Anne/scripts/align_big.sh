4be1fc1e5fd558627ae6e0b4abd73bc386342ab7
Anne <anne_van_ewijk@hotmail.com>
2021-10-25 09:54:47 +0200

    change align_big

--- Anne/scripts/align_big.sh
+++ Anne/scripts/align_big.sh
@@ -12,68 +12,43 @@
 #SBATCH --get-user-env=L
 
 NUMBER=5044
-FILE_NUM=SS600${NUMBER}_chr22.sort
+FILE_NUM=SS600${NUMBER}_test
 GENOOM=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/chr/unzip/chr22.fa #/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/chr/chrall.fa
-PATH=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/
+PATH_DIR=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/
 
 ml SAMtools/1.9-foss-2018b
 ml BWA/0.7.17-GCCcore-7.3.0
 ml Bowtie2/2.3.4.2-foss-2018b
 ml picard/2.20.5-Java-11-LTS
+ml FastQC/0.11.8-Java-11-LTS
+ml cutadapt/2.6-GCCcore-7.3.0-Python-3.7.4-bare
 
 echo "BEGIN"
-echo "${NUMBER}"
+#echo "${NUMBER}"
 
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}
-
-#cd /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292
-#samtools view -h ${FILE_NUM}.sorted.bam ${CHROM} > ${CHROM}/${NUMBER}/${FILE_NUM}.${CHROM}.sam
-
-
-
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/bowtie
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/bwa_aln
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/bwa_mem
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/QC
-
-#samtools view -bS ${FILE_NUM}.${CHROM}.sam > ${FILE_NUM}.${CHROM}.bam
-#samtools sort ${FILE_NUM}.${CHROM}.bam -o ${FILE_NUM}_${CHROM}.sort.bam
-#samtools sort -n ${FILE_NUM}.${CHROM}.bam -o ${FILE_NUM}_name_${CHROM}.sort.bam
+mkdir -p ${PATH_DIR}${NUMBER}/bowtie
+mkdir -p ${PATH_DIR}${NUMBER}/bwa_aln
+mkdir -p ${PATH_DIR}${NUMBER}/bwa_mem
+mkdir -p ${PATH_DIR}${NUMBER}/QC
 
 
-#SS6005044.sorted.bam
-#reads that mapped properly as pairs
-##samtools view -u -f 1 -F 12 ${PATH}${FILE_NUM}.sorted.bam > ${PATH}${NUMBER}/${FILE_NUM}_map_map.bam
-# R1 unmapped, R2 mapped
-##samtools view -u -f 4 -F 264 ${PATH}${FILE_NUM}.sorted.bam > ${PATH}${NUMBER}/${FILE_NUM}_unmap_map.bam
-# R1 mapped, R2 unmapped
-##samtools view -u -f 8 -F 260 ${PATH}${FILE_NUM}.sorted.bam > ${PATH}${NUMBER}/${FILE_NUM}_map_unmap.bam
-# R1 & R2 unmapped
-##samtools view -u -f 12 -F 256 ${PATH}${FILE_NUM}.sorted.bam > ${PATH}${NUMBER}/${FILE_NUM}_unmap_unmap.bam
-
-cd /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}
-# merge the three files
-##samtools merge -u ${PATH}${NUMBER}${FILE_NUM}_unmapped.bam ${PATH}${NUMBER}${FILE_NUM}_unmap_map.bam ${PATH}${NUMBER}${FILE_NUM}_map_unmap.bam ${PATH}${NUMBER}${FILE_NUM}_unmap_unmap.bam
+cd ${PATH_DIR}${NUMBER}
+#echo "BEGIN sort"
 # BAM files must be resorted so that they are ordered by read ID instead of location in the reference
-##samtools sort -n ${PATH}${NUMBER}${FILE_NUM}_map_map.bam -o ${PATH}${NUMBER}${FILE_NUM}_mapped.sort.bam
-##samtools sort -n ${PATH}${NUMBER}${FILE_NUM}_unmapped.bam -o ${PATH}${NUMBER}${FILE_NUM}_unmapped.sort.bam
-# it is a good idea to check that you have the correct number of reads and no redundancy. You can summarize the original BAM file to get an idea of where you started.
-##samtools flagstat ${PATH}${NUMBER}${FILE_NUM}.sorted.bam
-##samtools view -c ${PATH}${NUMBER}${FILE_NUM}_mapped.sort.bam
-##samtools view -c ${PATH}${NUMBER}${FILE_NUM}_unmapped.sort.bam
+samtools sort -n ${PATH_DIR}${FILE_NUM}.bam -o ${PATH_DIR}${NUMBER}/${FILE_NUM}_byName.sorted.bam
 # extract the FASTQ reads into two paired read files
-#    X bamToFastq -i SS6005044_chr22_mapped.sort.bam -fq SS6005044_chr22_mapped.1.fastq -fq2 SS6005044_chr22_mapped.2.fastq 
-#    X bamToFastq -i SS6005044_chr22_unmapped.sort.bam -fq SS6005044_chr22_unmapped.1.fastq -fq2 SS6005044_chr22_unmapped.2.fastq
-##samtools fastq -@ 8 ${PATH}${NUMBER}${FILE_NUM}_mapped.sort.bam -1 ${PATH}${NUMBER}${FILE_NUM}_mapped.1.fastq -2 ${PATH}${NUMBER}${FILE_NUM}_mapped.2.fastq -0 /dev/null -s /dev/null -n
-##samtools fastq -@ 8 ${PATH}${NUMBER}${FILE_NUM}_unmapped.sort.bam -1 ${PATH}${NUMBER}${FILE_NUM}_unmapped.1.fastq -2 ${PATH}${NUMBER}${FILE_NUM}_unmapped.2.fastq -0 /dev/null -s /dev/null -n
-samtools sort -n ${PATH}${FILE_NUM}.bam -o ${PATH}${NUMBER}${FILE_NUM}_byName.sorted.bam
+#echo "KLAAR sort"
+#echo "BEGIN 1 en 2"
+cd ${PATH_DIR}${NUMBER}
+samtools fastq -@ 8 ${PATH_DIR}${NUMBER}/${FILE_NUM}_byName.sorted.bam -1 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq -2 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq -0 /dev/null -s /dev/null -n
 
-samtools fastq -@ 8 ${PATH}${NUMBER}${FILE_NUM}_byName.sorted.bam -1 ${PATH}${NUMBER}${FILE_NUM}_name_R1.fastq -2 ${PATH}${NUMBER}${FILE_NUM}_name_R2.fastq -0 /dev/null -s /dev/null -n
-# combine both the first and paired reads together from the mapped and unmapped files
-##cat ${PATH}${NUMBER}${FILE_NUM}_mapped.1.fastq ${PATH}${NUMBER}${FILE_NUM}_unmapped.1.fastq > ${PATH}${NUMBER}${FILE_NUM}.1.fastq
-##cat ${PATH}${NUMBER}${FILE_NUM}_mapped.2.fastq ${PATH}${NUMBER}${FILE_NUM}_unmapped.2.fastq > ${PATH}${NUMBER}${FILE_NUM}.2.fastq
+fastqc -f fastq -o ${PATH_DIR}${NUMBER}/QC ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq
+fastqc -f bam -o ${PATH_DIR}${NUMBER}/QC ${PATH_DIR}${NUMBER}/${FILE_NUM}_byName.sorted.bam
 
-align_test() {
+#echo "KLAAR 1 en 2"
+
+# Last steps of alignment
+align_last_steps() {
     samtools view -Sb ${1}.sam > ${1}.bam
     samtools sort ${1}.bam -o ${1}_sort.bam
     samtools index ${1}_sort.bam
@@ -85,18 +60,20 @@ align_test() {
     samtools index ${1}.DR.bam
 }
 
+#echo "BWA mem"
 #BWA = Burrows-Wheeler Aligner
 #https://ucdavis-bioinformatics-training.github.io/2017-August-Variant-Analysis-Workshop/wednesday/alignment.html
-bwa mem ${GENOOM} ${PATH}${NUMBER}${FILE_NUM}_name_R1.fastq ${PATH}${NUMBER}${FILE_NUM}_name_R2.fastq > ${PATH}${NUMBER}/bwa_mem/mem_${FILE_NUM}.sam
-align_test ${PATH}${NUMBER}/bwa_mem/mem_${FILE_NUM}
+bwa mem ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq > ${PATH_DIR}${NUMBER}/bwa_mem/mem_${FILE_NUM}.sam
+align_last_steps ${PATH_DIR}${NUMBER}/bwa_mem/mem_${FILE_NUM}
 
-bwa aln ${GENOOM} ${PATH}${NUMBER}${FILE_NUM}_name_R1.fastq > ${PATH}${NUMBER}${FILE_NUM}_name_R1.sai && bwa aln ${GENOOM} ${PATH}${NUMBER}${FILE_NUM}_name_R2.fastq > ${PATH}${NUMBER}${FILE_NUM}_name_R2.sai && bwa sampe ${GENOOM} ${PATH}${NUMBER}${FILE_NUM}_name_R1.sai ${PATH}${NUMBER}${FILE_NUM}_name_R2.sai ${PATH}${NUMBER}${FILE_NUM}_name_R1.fastq ${PATH}${NUMBER}${FILE_NUM}_name_R2.fastq > ${PATH}${NUMBER}/bwa_aln/aln_${FILE_NUM}.sam
-align_test ${PATH}${NUMBER}/bwa_aln/aln_${FILE_NUM}
+#echo "BWA aln"
+bwa aln ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq > ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.sai && bwa aln ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq > ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.sai && bwa sampe ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.sai ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.sai ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq > ${PATH_DIR}${NUMBER}/bwa_aln/aln_${FILE_NUM}.sam
+align_last_steps ${PATH_DIR}${NUMBER}/bwa_aln/aln_${FILE_NUM}
 
 #BOWTIE2
-bowtie2-build ${GENOOM} ${PATH}${NUMBER}/bowtie/GENOOM
-bowtie2 -p 4 -x ${PATH}${NUMBER}/bowtie/GENOOM -1 ${PATH}${NUMBER}${FILE_NUM}_name_R1.fastq -2 ${PATH}${NUMBER}${FILE_NUM}_name_R2.fastq -S ${PATH}${NUMBER}/bowtie/bowtie2_${FILE_NUM}.sam
-align_test ${PATH}${NUMBER}/bowtie/bowtie2_${FILE_NUM}
-
+#echo "bowtie2"
+bowtie2-build ${GENOOM} ${PATH_DIR}${NUMBER}/bowtie/GENOOM
+bowtie2 -p 4 -x ${PATH_DIR}${NUMBER}/bowtie/GENOOM -1 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq -2 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq -S ${PATH_DIR}${NUMBER}/bowtie/bowtie2_${FILE_NUM}.sam
+align_last_steps ${PATH_DIR}${NUMBER}/bowtie/bowtie2_${FILE_NUM}
 
 echo "EIND"
--- Anne/test2.sh
+++ Anne/test2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-#SBATCH --job-name=testchr22alignment
-#SBATCH --output=testchr22alignment.out
-#SBATCH --error=testchr22alignment.err
-#SBATCH --time=23:59:59
+#SBATCH --job-name=align_big
+#SBATCH --output=align_big.out
+#SBATCH --error=align_big.err
+#SBATCH --time=167:59:59
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=96gb
 #SBATCH --nodes=1
@@ -12,64 +12,43 @@
 #SBATCH --get-user-env=L
 
 NUMBER=5044
-FILE_NUM=SS600${NUMBER}
-GENOOM=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/chr/chrall.fa
-#PATH=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${CHROM}/${NUMBER}
+FILE_NUM=SS600${NUMBER}_test
+GENOOM=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/chr/unzip/chr22.fa #/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/chr/chrall.fa
+PATH_DIR=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/
 
 ml SAMtools/1.9-foss-2018b
 ml BWA/0.7.17-GCCcore-7.3.0
 ml Bowtie2/2.3.4.2-foss-2018b
 ml picard/2.20.5-Java-11-LTS
+ml FastQC/0.11.8-Java-11-LTS
+ml cutadapt/2.6-GCCcore-7.3.0-Python-3.7.4-bare
 
 echo "BEGIN"
-echo "${NUMBER}"
+#echo "${NUMBER}"
 
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}
-
-#cd /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292
-#samtools view -h ${FILE_NUM}.sorted.bam ${CHROM} > ${CHROM}/${NUMBER}/${FILE_NUM}.${CHROM}.sam
-
-
-
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/bowtie
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/bwa_aln
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/bwa_mem
-mkdir /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}/QC
-
-#samtools view -bS ${FILE_NUM}.${CHROM}.sam > ${FILE_NUM}.${CHROM}.bam
-#samtools sort ${FILE_NUM}.${CHROM}.bam -o ${FILE_NUM}_${CHROM}.sort.bam
-#samtools sort -n ${FILE_NUM}.${CHROM}.bam -o ${FILE_NUM}_name_${CHROM}.sort.bam
+mkdir -p ${PATH_DIR}${NUMBER}/bowtie
+mkdir -p ${PATH_DIR}${NUMBER}/bwa_aln
+mkdir -p ${PATH_DIR}${NUMBER}/bwa_mem
+mkdir -p ${PATH_DIR}${NUMBER}/QC
 
 
-#reads that mapped properly as pairs
-samtools view -u -f 1 -F 12 ${FILE_NUM}.sorted.bam > /${NUMBER}/${FILE_NUM}_map_map.bam
-# R1 unmapped, R2 mapped
-samtools view -u -f 4 -F 264 ${FILE_NUM}.sorted.bam > /${NUMBER}/${FILE_NUM}_unmap_map.bam
-# R1 mapped, R2 unmapped
-samtools view -u -f 8 -F 260 ${FILE_NUM}.sorted.bam > /${NUMBER}/${FILE_NUM}_map_unmap.bam
-# R1 & R2 unmapped
-samtools view -u -f 12 -F 256 ${FILE_NUM}.sorted.bam > /${NUMBER}/${FILE_NUM}_unmap_unmap.bam
-
-cd /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/${NUMBER}
-# merge the three files
-samtools merge -u ${FILE_NUM}_unmapped.bam ${FILE_NUM}_unmap_map.bam ${FILE_NUM}_map_unmap.bam ${FILE_NUM}_unmap_unmap.bam
+cd ${PATH_DIR}${NUMBER}
+#echo "BEGIN sort"
 # BAM files must be resorted so that they are ordered by read ID instead of location in the reference
-samtools sort -n ${FILE_NUM}_map_map.bam -o ${FILE_NUM}_mapped.sort.bam
-samtools sort -n ${FILE_NUM}_unmapped.bam -o ${FILE_NUM}_unmapped.sort.bam
-# it is a good idea to check that you have the correct number of reads and no redundancy. You can summarize the original BAM file to get an idea of where you started.
-samtools flagstat ${FILE_NUM}.sorted.bam
-samtools view -c ${FILE_NUM}_mapped.sort.bam
-samtools view -c ${FILE_NUM}_unmapped.sort.bam
+samtools sort -n ${PATH_DIR}${FILE_NUM}.bam -o ${PATH_DIR}${NUMBER}/${FILE_NUM}_byName.sorted.bam
 # extract the FASTQ reads into two paired read files
-#    X bamToFastq -i SS6005044_chr22_mapped.sort.bam -fq SS6005044_chr22_mapped.1.fastq -fq2 SS6005044_chr22_mapped.2.fastq 
-#    X bamToFastq -i SS6005044_chr22_unmapped.sort.bam -fq SS6005044_chr22_unmapped.1.fastq -fq2 SS6005044_chr22_unmapped.2.fastq
-samtools fastq -@ 8 ${FILE_NUM}_mapped.sort.bam -1 ${FILE_NUM}_mapped.1.fastq -2 ${FILE_NUM}_mapped.2.fastq -0 /dev/null -s /dev/null -n
-samtools fastq -@ 8 ${FILE_NUM}_unmapped.sort.bam -1 ${FILE_NUM}_unmapped.1.fastq -2 ${FILE_NUM}_unmapped.2.fastq -0 /dev/null -s /dev/null -n
-# combine both the first and paired reads together from the mapped and unmapped files
-cat ${FILE_NUM}_mapped.1.fastq ${FILE_NUM}_unmapped.1.fastq > ${FILE_NUM}.1.fastq
-cat ${FILE_NUM}_mapped.2.fastq ${FILE_NUM}_unmapped.2.fastq > ${FILE_NUM}.2.fastq
+#echo "KLAAR sort"
+#echo "BEGIN 1 en 2"
+cd ${PATH_DIR}${NUMBER}
+samtools fastq -@ 8 ${PATH_DIR}${NUMBER}/${FILE_NUM}_byName.sorted.bam -1 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq -2 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq -0 /dev/null -s /dev/null -n
 
-align_test() {
+fastqc -f fastq -o ${PATH_DIR}${NUMBER}/QC ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq
+fastqc -f bam -o ${PATH_DIR}${NUMBER}/QC ${PATH_DIR}${NUMBER}/${FILE_NUM}_byName.sorted.bam
+
+#echo "KLAAR 1 en 2"
+
+# Last steps of alignment
+align_last_steps() {
     samtools view -Sb ${1}.sam > ${1}.bam
     samtools sort ${1}.bam -o ${1}_sort.bam
     samtools index ${1}_sort.bam
@@ -81,18 +60,20 @@ align_test() {
     samtools index ${1}.DR.bam
 }
 
+#echo "BWA mem"
 #BWA = Burrows-Wheeler Aligner
 #https://ucdavis-bioinformatics-training.github.io/2017-August-Variant-Analysis-Workshop/wednesday/alignment.html
-bwa mem ${GENOOM} ${FILE_NUM}_mapped.1.fastq ${FILE_NUM}_mapped.2.fastq > bwa_mem/mem_${FILE_NUM}.sam
-align_test bwa_mem/mem_${FILE_NUM}.sam
+bwa mem ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq > ${PATH_DIR}${NUMBER}/bwa_mem/mem_${FILE_NUM}.sam
+align_last_steps ${PATH_DIR}${NUMBER}/bwa_mem/mem_${FILE_NUM}
 
-bwa aln ${GENOOM} ${FILE_NUM}_mapped.1.fastq > ${FILE_NUM}_mapped.1.sai && bwa aln ${GENOOM} SS600${NUMBER}_mapped.2.fastq > SS600${NUMBER}_mapped.2.sai && bwa sampe ${GENOOM} SS600${NUMBER}_mapped.1.sai SS600${NUMBER}_mapped.2.sai SS600${NUMBER}_mapped.1.fastq SS600${NUMBER}_mapped.2.fastq > bwa_aln/aln_${FILE_NUM}.sam
-align_test bwa_aln/aln_${FILE_NUM}.sam
+#echo "BWA aln"
+bwa aln ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq > ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.sai && bwa aln ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq > ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.sai && bwa sampe ${GENOOM} ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.sai ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.sai ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq > ${PATH_DIR}${NUMBER}/bwa_aln/aln_${FILE_NUM}.sam
+align_last_steps ${PATH_DIR}${NUMBER}/bwa_aln/aln_${FILE_NUM}
 
 #BOWTIE2
-bowtie2-build ${GENOOM} GENOOM
-bowtie2 -p 4 -x GENOOM -1 ${FILE_NUM}_mapped.1.fastq -2 ${FILE_NUM}_mapped.2.fastq -S bowtie/bowtie2_${FILE_NUM}.sam
-align_test bowtie/bowtie2_${FILE_NUM}.sam
-
+#echo "bowtie2"
+bowtie2-build ${GENOOM} ${PATH_DIR}${NUMBER}/bowtie/GENOOM
+bowtie2 -p 4 -x ${PATH_DIR}${NUMBER}/bowtie/GENOOM -1 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R1.fastq -2 ${PATH_DIR}${NUMBER}/${FILE_NUM}_name_R2.fastq -S ${PATH_DIR}${NUMBER}/bowtie/bowtie2_${FILE_NUM}.sam
+align_last_steps ${PATH_DIR}${NUMBER}/bowtie/bowtie2_${FILE_NUM}
 
 echo "EIND"
