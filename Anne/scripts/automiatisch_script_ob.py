a406907928a71bc0a49423cd25bba0c8924b1ce0
Anne <anne_van_ewijk@hotmail.com>
2021-10-26 14:57:46 +0200

    add comments

--- Anne/scripts/automiatisch_script_ob.py
+++ Anne/scripts/automiatisch_script_ob.py
@@ -41,6 +41,7 @@ class Sample:
         """
         # Make run objects
         one_run = Run(row['sample_num'], row['type_sample'], row['name'], row['category'])
+        # Names sample_name
         self.sample_name = row['sample_num']
         # Checked under which category the run falls and adds it to that specific list
         if row['category'] == 'tumor':
@@ -50,55 +51,82 @@ class Sample:
 
     def get_arguments(self, head_path):
         """
-
-        :param head_path:
+        Causes all arguments to Mutect2/FilterMutectCalls to be made with all runs of a sample.
+        :param head_path:   Main path to where the file will be saved
         :return:
         """
+        # Starting string for file name/directory name
         arg = f'{self.sample_name}_'
+        # Starting string for arg_mutect2 (the whole argument to eventually run Mutect2)
         arg_mutect2 = ''
+        # Add each tumor to the different parameters
         for tum in self.tumors:
+            # Add tumor to arg (file name/directory name)
             arg += f'{tum.name}_'
+            # For example, makes SS6005044 -> 5044
             number_tum = tum.name.replace("SS600", "")
+            # Add tumor to arg_mutect2 (the whole argument to eventually run Mutect2)
             arg_mutect2 += f'-I {head_path}{number_tum}/SN_{tum.name}.DR.bam '
+        # Add each hc to the different parameters
         for hc in self.hc:
+            # Add hc to arg (file name/directory name)
             arg += f'{hc.name}_'
+            # For example, makes SS6005042 -> 5042
             number_hc = hc.name.replace("SS600", "")
+            # Add hc to arg_mutect2 (the whole argument to eventually execute Mutect2)
             arg_mutect2 += f'-I {head_path}{number_hc}/SN_{hc.name}.DR.bam -normal {hc.name}.DR.bam '
-
+        # Pastes everything together and makes three good arguments that can be passed to Mutect2/FilterMutectCalls
         arg_mutect2 += f'-O {head_path}{arg[:-1]}/unfiltered_{arg}somatic.vcf.gz'
         filter_mutect_calls_input = f'\n{head_path}{arg[:-1]}/unfiltered_{arg[:-1]}'
         filter_mutect_calls_output = f'\n{head_path}{arg[:-1]}/filtered_{arg[:-1]}'
+        # File name (and path) after which these arguments are written
         name_file = f'{head_path}{arg[:-1]}.txt'
+        # Calls the function that actually writes the arguments
         self.write_file(name_file, [arg_mutect2, filter_mutect_calls_input, filter_mutect_calls_output])
 
     def get_arguments_per_two(self, head_path):
         """
-
-        :param head_path:
+        Ensures that all arguments for Mutect2/FilterMutectCalls are made with all possible combinations
+        tumor vs hc of a sample (i.e. per two).
+        :param head_path:   Main path to where the file will be saved
         :return:
         """
         for hc in self.hc:
+            # For example, makes SS6005042 -> 5042
             number_hc = hc.name.replace("SS600", "")
             for tum in self.tumors:
+                # For example, makes SS6005044 -> 5044
                 number_tum = tum.name.replace("SS600", "")
+                # Add one tumor and one hc to arg (file name/directory name)
                 arg = f'{self.sample_name}_{tum.name}_{hc.name}'
-                arg_mutect2 = f'-I {head_path}{number_tum}/SN_{tum.name}.DR.bam -I {head_path}{number_hc}/SN_{hc.name}.DR.bam -normal {hc.name}.DR.bam -O {head_path}{arg}/unfiltered_{arg}_somatic.vcf.gz'
+                # Add one tumor and one hc to arg_mutect2 (the whole argument to eventually run Mutect2)
+                arg_mutect2 = f'-I {head_path}{number_tum}/SN_{tum.name}.DR.bam ' \
+                              f'-I {head_path}{number_hc}/SN_{hc.name}.DR.bam ' \
+                              f'-normal {hc.name}.DR.bam ' \
+                              f'-O {head_path}{arg}/unfiltered_{arg}_somatic.vcf.gz'
+                # Pastes everything together and makes 3 good arguments that can be passed to Mutect2/FilterMutectCalls
                 filter_mutect_calls_input = f'\n{head_path}{arg}/unfiltered_{arg}'
                 filter_mutect_calls_output = f'\n{head_path}{arg}/filtered_{arg}'
+                # File name (and path) after which these arguments are written
                 name_file = f'{head_path}{arg}.txt'
+                # Calls the function that actually writes the arguments
                 self.write_file(name_file, [arg_mutect2, filter_mutect_calls_input, filter_mutect_calls_output])
 
     def write_file(self, name_file, list_args):
         """
-
-        :param name_file:
-        :param list_args:
+        Writes the arguments (list_args) to a file.
+        :param name_file:   File name (and path) after which the arguments are written
+        :param list_args:   list of arguments with the following arguments:
+                            arg_mutect2:                    Argument to run Mutect2
+                            filter_mutect_calls_input:      input file for FilterMutectCalls (after Mutect2)
+                            filter_mutect_calls_output:     output file for FilterMutectCalls (after Mutect2)
         :return:
         """
-        f = open(name_file, "w")
+        file_arguments = open(name_file, "w")
+        # Loop over all arguments and writes these arguments line by line in a file
         for arg in list_args:
-            f.write(f'{arg}')
-        f.close()
+            file_arguments.write(f'{arg}')
+        file_arguments.close()
 
 
 def filter_file(path_file):
@@ -121,49 +149,60 @@ def filter_file(path_file):
     return df_selection
 
 
-def make_objects(df_selection, dict_sample, head_path):
+def make_objects(df_selection, head_path):
     """
     Creates the dictionary with the objects per participant/sample
     :param df_selection:    the custom data frame from which to create objects and a dictionary
-    :param dict_sample:     a dictionary containing the sample_num as key and as value Sample objects
-    :return:
+    :param head_path:       Main path to where the file will be saved
+    :return:                a dictionary containing the sample_num as key and as value Sample objects
     """
+    # Empty dict:
+    dict_samples = dict()
     # Loop over lines in data frame
     for index, row in df_selection.iterrows():
         # Checked if a key is already in the dictionary or not
-        if row['sample_num'] in dict_sample.keys():
+        if row['sample_num'] in dict_samples.keys():
             # update object Sample
-            dict_sample[row['sample_num']].set_category(row)
+            dict_samples[row['sample_num']].set_category(row)
         else:
             # Make object Sample
             one_sample = Sample()
             one_sample.set_category(row)
-            dict_sample[row['sample_num']] = one_sample
+            dict_samples[row['sample_num']] = one_sample
+    return dict_samples
 
-    # for i in dict_sample['S2'].tumors:
-    #     print(f'{i.sample_num} - {i.name}')
-    # for i in dict_sample['S2'].hc:
-    #     print(f'{i.sample_num} - {i.name}')
 
-    #for key, value in d.items():
-    for key in dict_sample:
-        dict_sample[key].get_arguments(head_path)
-        dict_sample[key].get_arguments_per_two(head_path)
+def arguments_to_file(dict_samples, head_path):
+    """
+    Ensures that all arguments are created and written.
+    :param dict_samples:     a dictionary containing the sample_num as key and as value Sample objects
+    :param head_path:       Main path to where the file will be saved
+    :return:
+    """
+    # Loop over keys from dict_sample
+    for key in dict_samples:
+        dict_samples[key].get_arguments(head_path)
+        dict_samples[key].get_arguments_per_two(head_path)
 
 
 def main():
     """
-    
+
     :return:
     """
-    #path_file = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/EGAD00001000292_metadata/delimited_maps/Sample_File.map"
-    #head_path = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/chr22/"
+    # the path to the file
+    # path_file = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/EGAD00001000292_metadata/delimited_maps/Sample_File.map"
+    # Main path to where the file will be saved
+    # head_path = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/chr22/"
+    # the path to the file
     path_file = "D:/Hanze_Groningen/STAGE/non-coding-somatic-mutations-in-cancer/Anne/data/EGAD00001000292_Sample_File.map"
+    # Main path to where the file will be saved
     head_path = "D:/Hanze_Groningen/STAGE/non-coding-somatic-mutations-in-cancer/Anne/data/"
+    # Filter file
     df_selection = filter_file(path_file)
-    print(df_selection.head())
-    dict_sample = dict()
-    make_objects(df_selection, dict_sample, head_path)
+    # Make objects for all samples/participants
+    dict_samples = make_objects(df_selection, head_path)
+    arguments_to_file(dict_samples, head_path)
 
 
 if __name__ == "__main__":
