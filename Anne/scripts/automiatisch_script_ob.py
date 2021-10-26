1796ce451acb3b590e53890548b8320232c2bb13
Anne <anne_van_ewijk@hotmail.com>
2021-10-26 14:15:44 +0200

    update ob

--- Anne/scripts/automiatisch_script_ob.py
+++ Anne/scripts/automiatisch_script_ob.py
@@ -31,6 +31,7 @@ class Sample:
         """
         self.tumors = list()
         self.hc = list()
+        self.sample_name = ''
 
     def set_category(self, row):
         """
@@ -40,14 +41,70 @@ class Sample:
         """
         # Make run objects
         one_run = Run(row['sample_num'], row['type_sample'], row['name'], row['category'])
+        self.sample_name = row['sample_num']
         # Checked under which category the run falls and adds it to that specific list
         if row['category'] == 'tumor':
             self.tumors.append(one_run)
         elif row['category'] == 'hc':
             self.hc.append(one_run)
 
+    def get_arguments(self, head_path):
+        """
 
-def prepair_file(path_file):
+        :param head_path:
+        :return:
+        """
+        arg = f'{self.sample_name}_'
+        arg_mutect2 = ''
+        for tum in self.tumors:
+            arg += f'{tum.name}_'
+            number_tum = tum.name.replace("SS600", "")
+            arg_mutect2 += f'-I {head_path}{number_tum}/SN_{tum.name}.DR.bam '
+        for hc in self.hc:
+            arg += f'{hc.name}_'
+            number_hc = hc.name.replace("SS600", "")
+            arg_mutect2 += f'-I {head_path}{number_hc}/SN_{hc.name}.DR.bam -normal {hc.name}.DR.bam '
+
+        arg_mutect2 += f'-O {head_path}{arg[:-1]}/unfiltered_{arg}somatic.vcf.gz'
+        filter_mutect_calls_input = f'\n{head_path}{arg[:-1]}/unfiltered_{arg[:-1]}'
+        filter_mutect_calls_output = f'\n{head_path}{arg[:-1]}/filtered_{arg[:-1]}'
+        name_file = f'{head_path}{arg[:-1]}.txt'
+        self.write_file(name_file, [arg_mutect2, filter_mutect_calls_input, filter_mutect_calls_output])
+
+    def get_arguments_per_two(self, head_path):
+        """
+
+        :param head_path:
+        :return:
+        """
+        for hc in self.hc:
+            number_hc = hc.name.replace("SS600", "")
+            for tum in self.tumors:
+                number_tum = tum.name.replace("SS600", "")
+                arg = f'{self.sample_name}_{tum.name}_{hc.name}'
+                arg_mutect2 = f'-I {head_path}{number_tum}/SN_{tum.name}.DR.bam \
+                    -I {head_path}{number_hc}/SN_{hc.name}.DR.bam \
+                        -normal {hc.name}.DR.bam \
+                            -O {head_path}{arg}/unfiltered_{arg}_somatic.vcf.gz'
+                filter_mutect_calls_input = f'\n{head_path}{arg}/unfiltered_{arg}'
+                filter_mutect_calls_output = f'\n{head_path}{arg}/filtered_{arg}'
+                name_file = f'{head_path}{arg}.txt'
+                self.write_file(name_file, [arg_mutect2, filter_mutect_calls_input, filter_mutect_calls_output])
+
+    def write_file(self, name_file, list_args):
+        """
+
+        :param name_file:
+        :param list_args:
+        :return:
+        """
+        f = open(name_file, "w")
+        for arg in list_args:
+            f.write(f'{arg}\n')
+        f.close()
+
+
+def filter_file(path_file):
     """
     Creates a new dataframe with the information needed to create objects
     :param path_file:       the path to the file
@@ -93,10 +150,13 @@ def make_objects(df_selection, dict_sample):
 
 
 def main():
-    # path_file = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/EGAD00001000292_metadata/delimited_maps/Sample_File.map"
-    # head_path = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/chr22/"
-    path_file = "D:/Hanze_Groningen/STAGE/non-coding-somatic-mutations-in-cancer/Anne/data/EGAD00001000292_Sample_File.map"
-    df_selection = prepair_file(path_file)
+    """
+    
+    :return:
+    """
+    path_file = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/EGAD00001000292_metadata/delimited_maps/Sample_File.map"
+    head_path = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/chr22/"
+    df_selection = filter_file(path_file)
     print(df_selection.head())
     dict_sample = dict()
     make_objects(df_selection, dict_sample)
