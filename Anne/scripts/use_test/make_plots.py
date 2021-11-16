d8e2c416696e6751cf74c9e0b491429af5b42d3b
Anne <anne_van_ewijk@hotmail.com>
2021-11-16 08:40:24 +0100

    scripts gisteren

--- Anne/scripts/use_test/make_plots.py
+++ Anne/scripts/use_test/make_plots.py
@@ -1,67 +1,72 @@
 #!/usr/bin/env python3
+# importing os.path module
+import os.path
 import pandas as pd
 import io
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
 
-#sys.argv[1] = path file
-#sys.argv[2] = 
-#sys.argv[3] = 
-path = "/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/samples/S1/compare_4094_4099/bowtie/0001.vcf"
+path = sys.argv[1]
+df = pd.read_csv(path, sep='\t', index_col=0)
+basename = os.path.basename(path).split('.')[0]
+print(basename)
+print(df.head())
 
-# https://gist.github.com/dceoy/99d976a2c01e7f0ba1c813778f9db744
-def read_vcf(path):
-    with open(path, 'r') as f:
-        lines = [l for l in f if not l.startswith('##')]
-    return pd.read_csv(
-        io.StringIO(''.join(lines)),
-        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
-               'QUAL': str, 'FILTER': str, 'INFO': str},
-        sep='\t'
-    ).rename(columns={'#CHROM': 'CHROM'})
 
-df = read_vcf(sys.argv[1].strip())
-df_only_samples = df.iloc[:,df.columns.get_loc("FORMAT"):]
-name = df_only_samples.columns[-1]
-print(name)
-print(df_only_samples.columns)
+def make_hist(data, column, basename, titles_dict):
+    fig = plt.figure(figsize=(15, 15))
+    sns.histplot(data=data, x=column).set_title(f'{column}')
+    fig.suptitle(f'{titles_dict[column]}', fontsize=20)
+    plt.yscale('log')
+    plt.savefig(f'{sys.argv[2]}{basename}_{column}_seaborn_hist.png')
+    plt.clf()
 
-set_format = set()
-for index, row in df_only_samples.iterrows():
-    list_format = row['FORMAT'].split(':')
-    set_format.update(list_format)
-print(set_format)
 
-df_format_value = pd.DataFrame(columns = list(set_format))
-for index, row in df_only_samples.iterrows():
-    list_values=list()
-    # dict_row = dict()
-    names_format = row['FORMAT'].split(':')
-    values_format = row[1].split(':')    
-    for i in list(set_format):
-        if i in names_format:
-            index_value = names_format.index(i)
-            # dict_row[i]=  values_format[index_value]  
-            list_values.append(values_format[index_value] )
+titles_dict = {'AD': 'Allelic depths (AD)', 'AF': 'Allele fractions (AF)', 'DP': 'read depth (DP)',
+              'F1R2': 'Count of reads in F1R2 pair orientation supporting each allele (F1R2)',
+              'F2R1': 'Count of reads in F2R1 pair orientation supporting each allele (F2R1)', 'GT': 'Genotype (GT)',
+              'PGT': 'Physical phasing haplotype information (PGT)',
+              'PS': 'Phasing set (PS) (typically the position of the first variant in the set)',
+              'SB': "Per-sample component statistics which comprise the Fisher's Exact Test to detect strand bias. (SB)"}
+
+for column in list(df.columns):
+    print(column)
+    data = df[df[column].notna()]
+    if column in ['AD', 'AF', 'F2R1', 'F1R2', 'SB']:
+        # cols = ['REF', 'ALT']
+        AD = df[column].str.split(',', expand=True)
+        data = AD.apply(pd.to_numeric)
+        # df[cols] = df['AD'].str.split(',', expand=True)
+        # data[cols] = data[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+        f, axes = plt.subplots(len(data.columns), 1, figsize=(20, 20), sharey=False)
+        f.tight_layout(pad=8.0)
+        for i, col in enumerate(data.columns):
+            if not data.empty:
+                # plt.figure(figsize=(15, 15))
+                ax = sns.histplot(data=data, x=col, ax=axes[i])
+                ax.set_yscale('log')
+                if column == 'AD':
+                    ax.set_xlabel('AD (Allelic depths)')
+                    if i == 0:
+                        ax.title.set_text("REF")
+                    else:
+                        ax.title.set_text(f"ALT{i}")
+                # plt.yscale('log')
+            else:
+                print(f'{col} EMPTY')
+        # plt.xlabel(f"AD (Allelic depths)")
+        # plt.ylabel(f"Counts")
+        f.suptitle(f'{titles_dict[column]}', fontsize=20)
+        plt.savefig(
+            f'{sys.argv[2]}{basename}_{column}_seaborn_hist.png')
+        plt.clf()
+    elif column == 'PID':
+        print(column)
+    else:
+        if column in ['DP']:
+            data[column] = pd.to_numeric(data[column], downcast="float")
+        if not data.empty:
+            make_hist(data, column, basename, titles_dict)
         else:
-            list_values.append(float("NaN"))
-            # dict_row[i] = '.'
-    df_format_value.loc[len(df_format_value)] = list_values
-    # df_format_value = df_format_value.append(dict_row, ignore_index=True)
-print(df_format_value.head())
-df_format_value.to_csv(f'{sys.argv[2]}_{name}.csv', sep='\t', encoding='utf-8')
-
-# for column in list(df_format_value.columns):
-#     data = df_format_value[df_format_value[column].notna()]
-#     if column in ['AF', 'DP']:
-#         print(data[column][25:30])
-#         data[column] = pd.to_numeric(data[column], downcast="float")
-#         if not data.empty:    
-#             plt.figure(figsize=(15, 15))
-#             sns.histplot(data=data, x=column).set_title(f'{column}')
-#             plt.yscale('log')
-#             plt.savefig(f'/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/datasets/EGAD00001000292/samples/FORMAT/{column}_seaborn_hist.png')
-#             plt.clf()
-#         else:
-#             print(f'{column} EMPTY')
+            print(f'{column} EMPTY')
