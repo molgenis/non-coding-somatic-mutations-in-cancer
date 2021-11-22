db7025a28dd3b91ca5be72212a7635d585578ecb
Anne <anne_van_ewijk@hotmail.com>
2021-11-22 16:57:07 +0100

    fix annotate

--- Anne/scripts/use_test/annotate.sh
+++ Anne/scripts/use_test/annotate.sh
@@ -11,15 +11,15 @@ OUTPUT_PATH=${PATH_GENERAL}annotated/
 for filename in ${PATH_GENERAL}/*.vcf; do
     echo ${filename}
     echo "$(basename -- $filename)"
-    # BASENAME=$( echo "$(basename -- $filename)")
-    # bcftools view ${filename} -Oz -o ${filename}.gz
-    # bcftools index ${filename}.gz
-    # #-c CHROM,FROM,TO,ID 
-    # bcftools annotate -c CHROM,FROM,TO,ID -a /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/dbSNP/merge_All_20180423.vcf.gz   -o ${OUTPUT_PATH}m_${BASENAME}  ${filename}.gz
+    BASENAME=$( echo "$(basename -- $filename)")
+    bcftools view ${filename} -Oz -o ${filename}.gz
+    bcftools index ${filename}.gz
+    #-c CHROM,FROM,TO,ID 
+    bcftools annotate -c CHROM,FROM,TO,ID -a /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/dbSNP/merge_All_20180423.vcf.gz   -o ${OUTPUT_PATH}${BASENAME}  ${filename}.gz
 
-    bcftools view --header-only ${OUTPUT_PATH}m_${BASENAME} > ${OUTPUT_PATH}dbSNP_filter/header_${BASENAME}
-    python3 filter_dbSNP.py ${OUTPUT_PATH}m_${BASENAME} ${OUTPUT_PATH}dbSNP_filter/
-    cat ${OUTPUT_PATH}dbSNP_filter/header_${BASENAME} ${OUTPUT_PATH}dbSNP_filter/noHeader_${BASENAME} >> ${OUTPUT_PATH}dbSNP_filter/merge_${BASENAME}
+    bcftools view --header-only ${OUTPUT_PATH}${BASENAME} > ${PATH_GENERAL}dbSNP_filter/header_${BASENAME}
+    python3 ${PATH_GENERAL}filter_dbSNP.py ${OUTPUT_PATH}${BASENAME} ${PATH_GENERAL}dbSNP_filter/
+    cat ${PATH_GENERAL}dbSNP_filter/header_${BASENAME} ${PATH_GENERAL}dbSNP_filter/noHeader_${BASENAME} >> ${PATH_GENERAL}dbSNP_filter/merge_${BASENAME}
 done
 
 echo 'END'
