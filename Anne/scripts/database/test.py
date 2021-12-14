2379ad966ec56ccadcec061edd2f0479d8d0b311
Anne <anne_van_ewijk@hotmail.com>
2021-12-14 16:11:23 +0100

    changes

--- Anne/scripts/database/test.py
+++ Anne/scripts/database/test.py
@@ -30,9 +30,10 @@ USE `internship` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `internship`.`project` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `project_id` VARCHAR(45) NULL DEFAULT NULL,
+  `project_id` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  UNIQUE INDEX `project_id_UNIQUE` (`project_id` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -43,10 +44,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `internship`.`donor` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `donor_ID` VARCHAR(45) NULL DEFAULT NULL,
+  `donor_ID` VARCHAR(45) NOT NULL,
   `project_ID` INT NOT NULL,
   PRIMARY KEY (`ID`, `project_ID`),
   UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  UNIQUE INDEX `donor_ID_UNIQUE` (`donor_ID` ASC) VISIBLE,
   INDEX `fk_donor_project_idx` (`project_ID` ASC) VISIBLE,
   CONSTRAINT `fk_donor_project`
     FOREIGN KEY (`project_ID`)
@@ -91,14 +93,10 @@ CREATE TABLE IF NOT EXISTS `internship`.`donor_has_snp` (
   INDEX `fk_donor_has_snp_donor1_idx` (`donor_ID` ASC, `donor_project_ID` ASC) VISIBLE,
   CONSTRAINT `fk_donor_has_snp_donor1`
     FOREIGN KEY (`donor_ID` , `donor_project_ID`)
-    REFERENCES `internship`.`donor` (`ID` , `project_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `internship`.`donor` (`ID` , `project_ID`),
   CONSTRAINT `fk_donor_has_snp_snp1`
     FOREIGN KEY (`snp_ID`)
-    REFERENCES `internship`.`snp` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `internship`.`snp` (`ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -107,6 +105,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
 
 """
 
@@ -124,41 +123,40 @@ path = "D:/Hanze_Groningen/STAGE/DIFFERENT CANCERS/db/BOCA-UK_db.tsv"
 df = pd.read_csv(path, sep='\t')
 print(df.columns)
 
-for project_id in list(set(df['project_id'])):
-    mycursor.execute("""INSERT INTO project (project_id)
-                  VALUES ('%s')""" % (str(project_id)))
-    last_id_project = mycursor.lastrowid
-    print(last_id_project)  
-    select_project = df.loc[df['project_id'] == project_id]
-    print(len(set(select_project['donor_id'])))  
-    for donor_id in list(set(select_project['donor_id'])):        
-        mycursor.execute("""INSERT INTO donor (donor_ID, project_ID)
-                  VALUES ('%s', %s)""" % (str(donor_id), int(last_id_project)))
-        # mydb.commit()
-        
-    
-        select_donor = df.loc[df['donor_id'] == donor_id]
-        for index, row in df.iterrows():
-             mycursor.execute("""INSERT INTO snp (chr, pos_start, pos_end, ref, alt, genome_version, depth, GLOC, platform, seq_strategy, tissue_id)
-                  VALUES ('%s', %s, %s, '%s', '%s', '%s', %s, '%s', '%s', '%s', '%s')""" % (str(row['chr']), int(row['pos_start']), int(row['pos_end']),
-                   str(row['ref']), str(row['alt']), str(row['genome_version']), int(row['depth']), str('int'), str(row['platform']), str(row['seq_strategy']), str(row['tissue_id'])))
-        #     sql3 = "INSERT INTO snp (   , depth, GLOC, platform, seq_strategy, tissue_id) VALUES (%s, %d, %d, %s, %s, %s, %d, %s, %s, %s, %s)"
-        #     val3 = (row['chr'], row['pos_start'], row['pos_end'], row['ref'], row['alt'], row['genome_version'],
-        #            row['depth'], 'int', row['platform'], row['seq_strategy'], row['tissue_id'])
-        #     mycursor.executemany(sql3, val3)
+def per_row():
+  for project_id in list(set(df['project_id'])):
+      mycursor.execute("""INSERT INTO project (project_id)
+                    VALUES ('%s')""" % (str(project_id)))
+      last_id_project = mycursor.lastrowid
+      print(last_id_project)  
+      select_project = df.loc[df['project_id'] == project_id]
+      print(len(set(select_project['donor_id'])))  
+      for donor_id in list(set(select_project['donor_id']))[:2]:        
+          mycursor.execute("""INSERT INTO donor (donor_ID, project_ID)
+                    VALUES ('%s', %s)""" % (str(donor_id), int(last_id_project)))
+          last_id_donor = mycursor.lastrowid
+          
 
-    mydb.commit()
-    mycursor.execute('SELECT * FROM donor')
-    for x in mycursor:
-        print(x)
+          select_donor = df.loc[df['donor_id'] == donor_id]
+          for index, row in df.iterrows():
+            if index < 20:
+              print(index)
+              # TODO KIJKEN HOE JE DEZELFDE SNPS MOET OPSLAAN
+              mycursor.execute("""INSERT INTO snp (chr, pos_start, pos_end, ref, alt, genome_version, depth, GLOC, platform, seq_strategy, tissue_id)
+                    VALUES ('%s', %s, %s, '%s', '%s', '%s', %s, '%s', '%s', '%s', '%s')""" % (str(row['chr']), int(row['pos_start']), int(row['pos_end']),
+                    str(row['ref']), str(row['alt']), str(row['genome_version']), int(row['depth']), str('int'), str(row['platform']), str(row['seq_strategy']), str(row['tissue_id'])))
+              last_id_snp = mycursor.lastrowid
+              mycursor.execute("""INSERT INTO donor_has_snp (donor_project_ID, donor_ID, snp_ID)
+                    VALUES (%s, %s, %s)""" % (int(last_id_project), int(last_id_donor), int(last_id_snp)))
+      mydb.commit()
+      mycursor.execute('SELECT * FROM donor')
+      for x in mycursor:
+          print(x)
 
+def per_tabel():
+  print()
 
-
-
-# mycursor.execute('DESCRIBE snp')
-
-# for x in mycursor:
-#     print(x)
+per_row()
 
 
 print('END')
