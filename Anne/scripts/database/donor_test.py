8f99c1b2242772ee6da7182870ddc47f2a7093ba
Anne <anne_van_ewijk@hotmail.com>
2021-12-22 10:22:21 +0100

    donor and sample

--- Anne/scripts/database/donor_test.py
+++ Anne/scripts/database/donor_test.py
@@ -6,6 +6,11 @@ path_files = f'{path}down*.tsv'
 # Loop over all files
 set_donor = set()
 list_donor = []
+set_samples = set()
+set_matched_sample = set()
+list_samples = []
+list_matched_sample = []
+
 for fname in glob.glob(path_files):
     print(fname)
     # Read file
@@ -15,8 +20,24 @@ for fname in glob.glob(path_files):
     list_donor.extend(list(set(df['icgc_donor_id'])))
     print(len(set_donor))
     print(len(list_donor))
+    print('-----')
+    set_samples.update(set(df['icgc_sample_id']))
+    list_samples.extend(list(set(df['icgc_sample_id'])))
+    print(len(set_donor))
+    print(len(list_donor))
+    print('-----')
+    set_matched_sample.update(set(df['matched_icgc_sample_id']))
+    list_matched_sample.extend(list(set(df['matched_icgc_sample_id'])))
+    print(len(set_donor))
+    print(len(list_donor))
 
 print('END')
 print(len(set_donor))
 print(len(list_donor))
+print('-----')
+print(len(set_samples))
+print(len(list_samples))
+print('-----')
+print(len(set_matched_sample))
+print(len(list_matched_sample))
 print('ENDENDENDENDEND')
