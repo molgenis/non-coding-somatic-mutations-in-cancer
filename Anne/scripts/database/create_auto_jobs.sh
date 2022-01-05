77b85162de8ea3260317a63e589299880453c4b8
Anne <anne_van_ewijk@hotmail.com>
2022-01-05 09:51:38 +0100

    test job

--- Anne/scripts/database/create_auto_jobs.sh
+++ Anne/scripts/database/create_auto_jobs.sh
@@ -11,38 +11,39 @@
 #SBATCH --export=NONE
 #SBATCH --get-user-env=L
 
+#seq FIRST STEP LAST
+chrom_num=($(seq 1 1 22))
 
-for ((i=1;i<=22;i++)); do
-    echo $i
-    echo "#!/usr/bin/bash" > file${i}.sh
-    echo "#SBATCH --job-name=file${i}" >> file${i}.sh
-    echo "#SBATCH --output=file${i}.out" >> file${i}.sh
-    echo "#SBATCH --error=file${i}.err" >> file${i}.sh
-    echo "#SBATCH --time=159:59:59" >> file${i}.sh
-    echo "#SBATCH --mem=96gb" >> file${i}.sh
-    echo "#SBATCH --nodes=1" >> file${i}.sh
-    echo "#SBATCH --open-mode=append" >> file${i}.sh
-    echo "#SBATCH --export=NONE" >> file${i}.sh
-    echo "#SBATCH --get-user-env=L" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "# Load ml" >> file${i}.sh
-    echo "ml Anaconda3/5.3.0" >> file${i}.sh
-    echo "source activate stage" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "echo 'HALLO ${i}'" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    echo "" >> file${i}.sh
-    
+chrom_num+=("X" "Y")
 
-
-    
-
-
+for chrom in "${chrom_num[@]}"
+do
+    echo ${chrom}
+    # echo $i
+    # echo "#!/usr/bin/bash" > file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "#SBATCH --job-name=file${i}" >> file${i}.sh
+    # echo "#SBATCH --output=file${i}.out" >> file${i}.sh
+    # echo "#SBATCH --error=file${i}.err" >> file${i}.sh
+    # echo "#SBATCH --time=159:59:59" >> file${i}.sh
+    # echo "#SBATCH --mem=96gb" >> file${i}.sh
+    # echo "#SBATCH --nodes=1" >> file${i}.sh
+    # echo "#SBATCH --open-mode=append" >> file${i}.sh
+    # echo "#SBATCH --export=NONE" >> file${i}.sh
+    # echo "#SBATCH --get-user-env=L" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "# Load ml" >> file${i}.sh
+    # echo "ml Anaconda3/5.3.0" >> file${i}.sh
+    # echo "source activate stage" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "echo 'HALLO ${i}'" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
+    # echo "" >> file${i}.sh
 done
