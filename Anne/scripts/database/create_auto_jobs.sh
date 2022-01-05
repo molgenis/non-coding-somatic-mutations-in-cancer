cad8d34002c12baaa9eebb53abb0e98ecba09a3a
Anne <anne_van_ewijk@hotmail.com>
2022-01-05 09:54:08 +0100

    test job

--- Anne/scripts/database/create_auto_jobs.sh
+++ Anne/scripts/database/create_auto_jobs.sh
@@ -16,34 +16,29 @@ chrom_num=($(seq 1 1 22))
 
 chrom_num+=("X" "Y")
 
-for chrom in "${chrom_num[@]}"
+for i in "${chrom_num[@]}"
 do
-    echo ${chrom}
-    # echo $i
-    # echo "#!/usr/bin/bash" > file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "#SBATCH --job-name=file${i}" >> file${i}.sh
-    # echo "#SBATCH --output=file${i}.out" >> file${i}.sh
-    # echo "#SBATCH --error=file${i}.err" >> file${i}.sh
-    # echo "#SBATCH --time=159:59:59" >> file${i}.sh
-    # echo "#SBATCH --mem=96gb" >> file${i}.sh
-    # echo "#SBATCH --nodes=1" >> file${i}.sh
-    # echo "#SBATCH --open-mode=append" >> file${i}.sh
-    # echo "#SBATCH --export=NONE" >> file${i}.sh
-    # echo "#SBATCH --get-user-env=L" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "# Load ml" >> file${i}.sh
-    # echo "ml Anaconda3/5.3.0" >> file${i}.sh
-    # echo "source activate stage" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "echo 'HALLO ${i}'" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
-    # echo "" >> file${i}.sh
+    echo $i
+    echo "#!/usr/bin/bash" > /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
+    echo "#SBATCH --job-name=file${i}" >> /jobs/file${i}.sh
+    echo "#SBATCH --output=/jobs/file${i}.out" >> /jobs/file${i}.sh
+    echo "#SBATCH --error=/jobs/file${i}.err" >> /jobs/file${i}.sh
+    echo "#SBATCH --time=159:59:59" >> /jobs/file${i}.sh
+    echo "#SBATCH --mem=96gb" >> /jobs/file${i}.sh
+    echo "#SBATCH --nodes=1" >> /jobs/file${i}.sh
+    echo "#SBATCH --open-mode=append" >> /jobs/file${i}.sh
+    echo "#SBATCH --export=NONE" >> /jobs/file${i}.sh
+    echo "#SBATCH --get-user-env=L" >> /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
+    echo "# Load ml" >> /jobs/file${i}.sh
+    echo "ml Anaconda3/5.3.0" >> /jobs/file${i}.sh
+    echo "source activate stage" >> /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
+    echo "echo 'HALLO ${i}'" >> /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
+    echo "" >> /jobs/file${i}.sh
 done
