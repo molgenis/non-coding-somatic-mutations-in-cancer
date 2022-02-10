cfef82396d940a2f09a051804fe9d1ba547d9384
Anne <anne_van_ewijk@hotmail.com>
2022-02-10 11:21:46 +0100

    add comments

--- Anne/scripts/new_plan/OVERVIEW_SNP.py
+++ Anne/scripts/new_plan/OVERVIEW_SNP.py
@@ -15,38 +15,58 @@ class OVERVIEW_SNP:
 
     def set_snp(self, chr, pos, ref, alt, donor_id, total_read_count, 
                 mutant_allele_read_count, icgc_specimen_id, project_code):
-        
+        """
+
+        """
+        # Create snp ID
         SNP_ID = f'{chr}_{pos}_{ref}_{alt}'
+        # Call check_homo_hetero
         homo_hetero, total_read_count, mutant_allele_read_count = self.check_homo_hetero(total_read_count, mutant_allele_read_count)
+        # Create format structure with values
         snp_format_donor = ':'.join([str(homo_hetero), str(total_read_count), str(mutant_allele_read_count), str(icgc_specimen_id), str(project_code)]) #[icgc_specimen_id, project_code, homo_hetero, total_read_count, mutant_allele_read_count]
-        
+        # Make a dictionary with the donor ids as keys and the format structure as values, but then for the donors who do not have that snp. 
+        # So they don't have homo_hetero, total_read_count, mutant_allele_read_count.
         if donor_id not in self.all_donors:
             self.all_donors[donor_id] = ':'.join([str('0/0'), str('.'), str('.'), str(icgc_specimen_id), str(project_code)])
         
-        if SNP_ID not in self.dict_SNP_ID:
-            snp = SNP(chr, pos, ref, alt)
-            self.dict_SNP_ID[SNP_ID] = snp
-        else:
-            snp = self.dict_SNP_ID[SNP_ID]
+        # Check if the snp id is not in the dict_SNP_ID. 
+        # If it doesn't exist, create the snp object.
+        # if SNP_ID not in self.dict_SNP_ID:
+        #     snp = SNP(chr, pos, ref, alt)
+        #     self.dict_SNP_ID[SNP_ID] = snp
+        # else:
+        #     snp = self.dict_SNP_ID[SNP_ID]
         
-
+        # Check if the snp id is in the vcf_dict. 
         if SNP_ID in self.vcf_dict:
+            # Check whether the donor_id already exists in vcf_dict[SNP_ID], 
+            # if not add this donor (with the format structure of this donor).
             if donor_id not in self.vcf_dict[SNP_ID]:
                 self.vcf_dict[SNP_ID][donor_id] = snp_format_donor
+        # If it doesn't exist, create the snp object.
+        # And add value to vcf_dict[SNP_ID]. vcf_dict[SNP_ID] is a dictionary that consists of the 
+        # standard columns of a vcf file as keys but also the donors with their format structure.
         else:
-            # new_donor_dict = dict()
-            # new_donor_dict[snp.donor_id] = [icgc_specimen_id, project_code, snp.homo_hetero]
-            # self.vcf_dict[SNP_ID] = {'SNP_INFO': snp, donor_id: snp_format_donor}
+            snp = SNP(chr, pos, ref, alt)
             self.vcf_dict[SNP_ID] = {'CHROM': snp.chr, 'POS': snp.pos, 'ID': snp.id, 'REF': snp.ref, 'ALT': snp.alt,
                                     'QUAL': snp.qual, 'FILTER': snp.filter, 'INFO': snp.info, 'FORMAT': snp.format,
                                     donor_id: snp_format_donor}
 
     def check_homo_hetero(self, total_read_count, mutant_allele_read_count):
+        """
+
+        """
+        # Check if mutant_allele_read_count and total_read_count are empty
+        # If these are not empty divide mutant_allele_read_count by total_read_count 
+        # and determine if they are homozygous or heterozygous.
         if mutant_allele_read_count != '' and total_read_count != '':
             if int(mutant_allele_read_count)/int(total_read_count) >= 0.9:
                 homo_hetero = '1/1'
             else:
                 homo_hetero = '0/1'
+        # If mutant_allele_read_count and total_read_count are empty, 
+        # change both variables with a '.'. 
+        # Make homo_hetero a '.' for now. # TODO TODO 
         else:
             if mutant_allele_read_count == '':
                 mutant_allele_read_count = '.'
@@ -56,9 +76,13 @@ class OVERVIEW_SNP:
         return homo_hetero, total_read_count, mutant_allele_read_count
 
     def get_vcf_dict(self):
+        """
+        """
         return self.vcf_dict
 
     def get_all_donors(self):
+        """
+        """
         return self.all_donors
 
 
--- Anne/scripts/new_plan/make_vcf.py
+++ Anne/scripts/new_plan/make_vcf.py
@@ -1,16 +1,24 @@
 import gzip
 from OVERVIEW_SNP import OVERVIEW_SNP
-from SNP import SNP
 
-project_file = gzip.open("D:/Hanze_Groningen/STAGE/NEW PLAN/ALL-US.tsv.gz",'rt')
+# Path to the file
+path_file = "D:/Hanze_Groningen/STAGE/NEW PLAN/ALL-US.tsv.gz"
+# Open and unzip file
+project_file = gzip.open(path_file,'rt')
+# Headers of the files
 header = project_file.readline().strip().split("\t")
 
+# Make overview object
 overview = OVERVIEW_SNP()
-
+# Loop over the lines
 for line in project_file:
+    # Strip the line
     line = line.strip()
+    # Split the line on tabs
     elems = line.split("\t")
+    # Check if column 33 (=sequencing_strategy) is 'WGS'
     if elems[33] == 'WGS':
+        # Get the needed information out of de elems
         donor_id = elems[1]
         project_code = elems[2]
         icgc_specimen_id = elems[3]
@@ -20,6 +28,7 @@ for line in project_file:
         alt = elems[16]
         total_read_count = elems[19]
         mutant_allele_read_count = elems[20]
+        # Call set_snp (function in the file: OVERVIEW_SNP)
         overview.set_snp(chr, pos, ref, alt, donor_id, total_read_count, 
                         mutant_allele_read_count, icgc_specimen_id, project_code)
 
@@ -27,19 +36,26 @@ for line in project_file:
 print(overview.get_vcf_dict())
 print(len(overview.get_all_donors()))
 
+# Get vcf_dict (key: SNP_ID, value: dict --> (key: vcf columns or donor_id, value: information out of columns or format structure of donor))
 vcf_dict = overview.get_vcf_dict()
+# Get all_donors (key: donor_id, value: list of format structure <-- but this is for when the donors hasn't that snp)
 all_donors = overview.get_all_donors()
 
+# Loop over keys/values of vcf_dict
 for SNP_ID, vcf_info in vcf_dict.items():
+    # Loop over keys/values all_donors
     for donor_ID, format_info in all_donors.items():
+        # Check if donor_id exists in vcf_info (value of vcf_dict)
+        # If it does not appear, add the donor with the empty format structure
         if donor_ID not in vcf_info:
             vcf_dict[SNP_ID][donor_ID] = format_info
 
+# Check whether all snps contain the same number of donors
 for key, value in vcf_dict.items():
     if len(value) != (len(all_donors) + 9):
         print(f'--------------{key}')
 
-print(overview.get_vcf_dict())
+# print(overview.get_vcf_dict())
 
 
 
