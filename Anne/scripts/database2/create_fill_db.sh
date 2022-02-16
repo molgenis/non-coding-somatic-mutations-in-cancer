c4cbb81b149d48374986fb934645d123d748c7c0
Anne <anne_van_ewijk@hotmail.com>
2022-02-16 12:24:49 +0100

    whole db

--- Anne/scripts/database2/create_fill_db.sh
+++ Anne/scripts/database2/create_fill_db.sh
@@ -16,7 +16,7 @@ source activate stage
 
 SCRIPT_PATH=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/non-coding-somatic-mutations-in-cancer/Anne/scripts/database2/
 PATH_DATA=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/cancer_data/new_data_db/ #todo /groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/cancer_copy/
-DB_NAME='Database_internship_UPDATE2.0'
+DB_NAME='00Database_internship_UPDATE2.0.db' #'Database_internship_UPDATE2.0'
 DATABASE_GENE=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/cancer_data/new_db/${DB_NAME}.db
 GENE_FILE=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/cancer_data/snp132_ucsc_hg19_checkGene.bed
 
