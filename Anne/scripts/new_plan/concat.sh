e6b8f856d2047ffcdbe3e6b14a488caec80988ea
Anne <anne_van_ewijk@hotmail.com>
2022-02-24 16:30:20 +0100

    add comments

--- Anne/scripts/new_plan/concat.sh
+++ Anne/scripts/new_plan/concat.sh
@@ -14,27 +14,30 @@
 PATH_NEW=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/GDC/merge_D/
 PATH_OLD=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/GDC/All_B/
 # PATH_OLD=/groups/umcg-wijmenga/tmp01/projects/lude_vici_2021/rawdata/GDC/All_H/
-listje=()
+codeList=()
 
 
 ml BCFtools/1.11-GCCcore-7.3.0
  
 
 for filename in ${PATH_OLD}*.vcf.gz*; do
-    #echo "$filename"
+    # Get basename
     BASENAME=$( echo "$(basename -- $filename)")
-    #echo ${BASENAME}
-    #sepName=$(echo $BASENAME | tr ".wgs" "\n")
-    IFS='.' read -r sepName string <<< "$BASENAME"
-    if [[ ! " ${listje[*]} " =~ " ${sepName} " ]]; then
-        bcftools index ${PATH_OLD}${sepName}.wgs.CaVEMan.raw_somatic_mutation.vcf.gz
-        bcftools index ${PATH_OLD}${sepName}.wgs.sanger_raw_pindel.raw_somatic_mutation.vcf.gz
-        bcftools concat -a -Oz ${PATH_OLD}${sepName}.wgs.CaVEMan.raw_somatic_mutation.vcf.gz ${PATH_OLD}${sepName}.wgs.sanger_raw_pindel.raw_somatic_mutation.vcf.gz > ${PATH_NEW}${sepName}.vcf.gz
-        echo 'JA'
-        echo ${sepName}
-        listje+=( ${sepName} )
+    # Get code (before .)
+    IFS='.' read -r codeName string <<< "$BASENAME"
+    # Check if codeName (=code) exist in codeList
+    # If it is not in the list it is added and the files are combined with that code.
+    if [[ ! " ${codeList[*]} " =~ " ${codeName} " ]]; then
+        # Index files
+        bcftools index ${PATH_OLD}${codeName}.wgs.CaVEMan.raw_somatic_mutation.vcf.gz
+        bcftools index ${PATH_OLD}${codeName}.wgs.sanger_raw_pindel.raw_somatic_mutation.vcf.gz
+        # Concat files
+        bcftools concat -a -Oz ${PATH_OLD}${codeName}.wgs.CaVEMan.raw_somatic_mutation.vcf.gz ${PATH_OLD}${codeName}.wgs.sanger_raw_pindel.raw_somatic_mutation.vcf.gz > ${PATH_NEW}${sepName}.vcf.gz
+        # echo 'JA'
+        # echo ${codeName}
+        codeList+=( ${codeName} )
     fi
     
 done
 
-echo ${listje[*]}
+# echo ${codeList[*]}
