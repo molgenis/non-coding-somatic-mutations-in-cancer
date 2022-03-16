5967c01fcd12103f4966c3d167cf5148c0064ad4
Anne <anne_van_ewijk@hotmail.com>
2022-03-16 10:39:26 +0100

    Gene script

--- Anne/scripts/new_plan2/Database.py
+++ Anne/scripts/new_plan2/Database.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 import sqlite3
-# import matplotlib.pyplot as plt
-import numpy as np
 
 
 class Database:
@@ -38,68 +36,3 @@ class Database:
         """
         self.cursor.close()
         self.mydb_connection.close()
-
-    def count_values(self, column, table, where, title):
-        """
-        Counts rows in the column that possibly meet a given 'where' statement in the database
-        :param column: The selected column of the table in the database
-        :param table:  The selected table in the database
-        :param where:  The 'where' statement in the SQL code
-        :return:
-        """
-        # Count the rows without the 'where' statement
-        self.cursor.execute(f"""
-                            SELECT {column}, COUNT(*)
-                            FROM {table}
-                            GROUP BY {column};
-                            """)
-        results = self.cursor.fetchall()
-        print(f'---{column}')
-        for res in results:
-            print(f'{res[0]} - {res[1]}')
-        print(f'FILTER: {where}')
-        # Count the rows with the 'where' statement
-        self.cursor.execute(f"""
-                            SELECT {column}, COUNT(*)
-                            FROM {table}
-                            WHERE {where}
-                            GROUP BY {column};
-                            """)
-        results = self.cursor.fetchall()
-        for res in results:
-            print(f'{res[0]} - {res[1]}')
-        # self.make_plot_true_false(results, title)
-        return results
-        
-
-    def make_plot_true_false(self, results, title):
-        if results[0][0] == 0:
-            names = ['False', 'True']
-        elif results[0][0] == 1:
-            names = ['True', 'False']
-        
-        if len(results) == 2:
-            values = [results[0][1], results[1][1]]
-        elif len(results) == 1:
-            values = [results[0][1], 0]
-
-
-        # plt.bar(names, values)
-        # # Add title and axis names
-        # plt.title(title)
-        # plt.xlabel('Categories')
-        # plt.ylabel('Counts')
-        #
-        # plt.show()
-
-        
-
-        # print(results[1][1])
-        # for index, res in enumerate(results):
-        #     if res[0] == 1:
-        #         #TRUE
-        #         plt.bar(res[0], res[1], color = 'g')
-        #     else:
-        #         plt.bar(res[0], res[1], color = 'r')
-        
-        # plt.show()
--- Anne/scripts/new_plan2/position_before.py
+++ Anne/scripts/new_plan2/position_before.py
@@ -1,18 +1,27 @@
 from Database import Database
 import pandas as pd
+# Python program to create
+# sparse matrix using csr_matrix()
+  
+# Import required package
+import numpy as np
+from scipy.sparse import csr_matrix
 
 def add_value(db):
-    # add dosages
+    """
+
+    """
+    # add in_transcript
     db.cursor.execute(f"""
                     ALTER TABLE snp
                     ADD `in_transcript` BOOLEAN DEFAULT(FALSE)
                     """)
-    # add dosages
+    # add in_coding
     db.cursor.execute(f"""
                     ALTER TABLE snp
                     ADD `in_coding` BOOLEAN DEFAULT(FALSE)
                     """)
-    # add dosages
+    # add in_exon
     db.cursor.execute(f"""
                     ALTER TABLE snp
                     ADD `in_exon` BOOLEAN DEFAULT(FALSE)
@@ -21,20 +30,23 @@ def add_value(db):
 
 
 
-def set_gene(db, index, row):
+def set_gene(db, index, row, chr):
+    """
+
+    """
     print(index)
     # Update in_transcript
     db.cursor.execute(
         """UPDATE snp
             SET in_transcript = TRUE
             WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s;""" %
-        (str(row['chrom'].replace('chr', '')), int(row['txStart']), int(row['txEnd'])))
+        (str(chr), int(row['txStart']), int(row['txEnd'])))
     # Update in_coding
     db.cursor.execute(
         """UPDATE snp
             SET in_coding = TRUE
             WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s;""" %
-        (str(row['chrom'].replace('chr', '')), int(row['cdsStart']), int(row['cdsEnd'])))
+        (str(chr), int(row['cdsStart']), int(row['cdsEnd'])))
     # Get start and end of the exons
     exon_start = row['exonStarts'].rstrip(',').split(',')
     exon_end = row['exonEnds'].rstrip(',').split(',')
@@ -45,141 +57,130 @@ def set_gene(db, index, row):
             """UPDATE snp
                 SET in_exon = TRUE
                 WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s;""" %
-            (str(row['chrom'].replace('chr', '')), int(exon_start[i]), int(exon_end[i])))
-    
+            (str(chr), int(exon_start[i]), int(exon_end[i])))
+    # Committing the current transactions
     db.mydb_connection.commit()
     
 
 def check_snp_id(db, snp_IDs, donor_dict):
+    """
+    
+    """
+    # Make donor_list
+    # List because double donors count
     donor_list = list()
+    # Loop over list with snp IDs
     for snp_ID in snp_IDs:
+        # Make donor_set
+        # Set because duplicate snps from a donor don't count
         donor_set = set()
+        # See how many snp_IDs there are in this table that are equal to the ID of the snp
         db.cursor.execute("""
                         SELECT snp_ID, donor_ID
                         FROM 'donor_has_snp'
-                        WHERE snp_ID = %s
+                        WHERE snp_ID = %s AND total_read_count > 0 AND mutant_allele_read_count > 0;
                         """ %
-            (snp_ID))
+            (snp_ID)) #TODO total read count and mutation groter dan 0
         results = db.cursor.fetchall()
         for res in results:
+            # Add donor_ID to the set
             donor_set.add(donor_dict[res['donor_ID']]) #res['donor_ID']
+        # Extend the list with the donor_set
         donor_list.extend(donor_set)
-    print(donor_list)
     return donor_list
 
 
-def close_to(db, gene, chr, start, end, position_gene, before_after_gene, donor_dict):
-    #START
-    # db.cursor.execute("""
-    #                 SELECT COUNT(ID)
-    #                 FROM 'snp'
-    #                 WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s
-    #                 """ %
-    #     (str(chr.replace('chr', '')), int(start)-position_gene, int(start)))
+def close_to(db, gene, chr, start, end, position_out_gene, position_in_gene, before_after_gene, donor_dict, donor_list, gene_name_bef, gene_name_aft, sparseMatrix_before, sparseMatrix_after):
+    # START
+    print('START')
+    gene_index_bef = gene_name_bef.index(gene)
+    gene_name_bef[gene_index_bef] = f'{gene}:chr{chr}:{int(start)-position_out_gene}-{int(start)+position_in_gene}'
     db.cursor.execute("""
                     SELECT ID
                     FROM 'snp'
                     WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s
                     """ %
-        (str(chr.replace('chr', '')), int(start)-position_gene, int(start)))
-    results = db.cursor.fetchall()
+        (str(chr), int(start)-position_out_gene, int(start)+position_in_gene))
+    results_before = db.cursor.fetchall()
+    # Make before_list
     before_list = list()
-    for res in results:
-        before_list.append(res['ID'])
+    print('append to list')
+    for res_bef in results_before:
+        # Add ID to before_list
+        before_list.append(res_bef['ID'])
+    # Call donor_list_before
+    print('check donors')
     donor_list_before = check_snp_id(db, before_list, donor_dict)
-    
-    db.cursor.execute("""
-                    SELECT ID
-                    FROM 'snp'
-                    WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s
-                    """ %
-        (str(chr.replace('chr', '')), int(end), int(end)+position_gene))
-    results = db.cursor.fetchall()
-    after_list = list()
-    for res in results:
-        after_list.append(res['ID'])
-    donor_list_after = check_snp_id(db, after_list, donor_dict)
+    print('loop donors')
+    for donor in donor_list_before:
+        donor_index = donor_list.index(donor)
+        sparseMatrix_before[donor_index, gene_index_bef] += 1
 
-    
-
-    before_after_gene.write(gene+'\t'+chr+'\t'+str(len(before_list))+'\t'+str(len(after_list))+'\t'+str(start)+'\t'+str(end)+'\t'+','.join(map(str,before_list))+'\t'+','.join(map(str,after_list))+'\t'+str(len(donor_list_before))+'\t'+str(len(donor_list_after))+'\t'+','.join(map(str,donor_list_before))+'\t'+','.join(map(str,donor_list_after))+'\n')
-    
-        
-        
-
-
-    
-    # #START
+    # # END
     # db.cursor.execute("""
-    #                 SELECT *
+    #                 SELECT ID
     #                 FROM 'snp'
     #                 WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s
     #                 """ %
-    #     (str(chr), int(start)-position_gene, int(start)))
-    # results = db.cursor.fetchall()
-    # start_set = set()
-    # for res in results:
-    #     start_set.add(res['ID'])
+    #     (str(chr), int(end)-position_in_gene, int(end)+position_out_gene))
+    # results_after = db.cursor.fetchall()
+    # # Make after_list
+    # after_list = list()
+    # for res_aft in results_after:
+    #     # Add ID to after_list
+    #     after_list.append(res_aft['ID'])
+    # # Call donor_list_after
+    # donor_list_after = check_snp_id(db, after_list, donor_dict)
+    # # Write to file
+    # before_after_gene.write(gene+'\t'+chr+'\t'+str(start)+'\t'+str(end)+'\t'+str(len(before_list))+'\t'+str(len(after_list))+'\t'+','.join(map(str,before_list))+'\t'+','.join(map(str,after_list))+'\t'+str(len(donor_list_before))+'\t'+str(len(donor_list_after))+'\t'+str(len(set(donor_list_before)))+'\t'+str(len(set(donor_list_after)))+'\t'+','.join(map(str,donor_list_before))+'\t'+','.join(map(str,donor_list_after))+'\n')
 
-    # count_number += len(list(start_set))
-    # # letters in a but not in set_snps
-    # unique_start = list(start_set - set(set_snps))
-    # count_unique += len(unique_start)
-    # set_snps.update(unique_start)
+    return sparseMatrix_before, sparseMatrix_after, gene_name_bef, gene_name_aft
 
+def write_sparse_matrix(sparseMatrix, gene_name_list, donor_list, save_path, pos):
+    df = pd.DataFrame(data=sparseMatrix, columns=gene_name_list)
+    df['donor_id'] = donor_list
+    # df['cancer'] = donor_cancer_list
+    df.set_index('donor_id')
+    df.to_csv(f'{save_path}sparsematrix_{pos}.tsv.gz', sep="\t", index=True, encoding='utf-8', 
+                compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1})
 
-    # #END
-    # db.cursor.execute("""
-    #                 SELECT *
-    #                 FROM 'snp'
-    #                 WHERE chr = '%s' AND pos_start >= %s AND pos_end <= %s
-    #                 """ %
-    #     (str(chr), int(end), int(end)+position_gene))
-    # results = db.cursor.fetchall()
-    # end_set = set()
-    # for res in results:
-    #     end_set.add(res['ID'])
-
-    # count_number += len(end_set)
-    # # letters in a but not in set_snps
-    # unique_end = list(end_set - set(set_snps))
-    # count_unique += len(unique_end)
-    # set_snps.update(unique_end)
-
-    # if count_number > 0:
-    #     print(count_number)
-    #     # print(set_snps)
-    #     print(count_unique)
-    #     print('-------')
-
-
-
-def loop_over_DOT(db, gene_df, position_gene, donor_dict):
+def loop_over_genes(db, gene_df, position_out_gene, position_in_gene, donor_dict, donor_list, save_path, gene_name_bef, gene_name_aft, sparseMatrix_before, sparseMatrix_after):
     #TODO Nu kunnen genen wel overlappen en dat uiteindelijk het gen wel binnen een ander gen ligt
     #TODO Het kan nu overlappen, er kan nu twee keer hetzelfde gen mee worden genomen
     #TODO
-    before_after_gene = open(f'D:/Hanze_Groningen/STAGE/db/gene_before_after_{position_gene}.tsv', 'w')
-    before_after_gene.write('gene\tchr\tbefore\tafter\tstart_position\tend_position\tbefore_list\tafter_list\tbefore_donor\tafter_donor\tbefore_donor_list\tafter_donor_list\n')
+    # Make file
+    before_after_gene = open(f'{save_path}gene_before_after_{position_out_gene}_{position_in_gene}.tsv', 'w')
+    # Write header
+    before_after_gene.write('gene\tchr\tstart_position\tend_position\tbefore\tafter\tbefore_list\tafter_list\tbefore_donor\tafter_donor\tbefore_donor_set\tafter_donor_set\tbefore_donor_list\tafter_donor_list\n')
+    # Loop over genes in file
     for index, row in gene_df.iterrows():
-        print(row['chrom'], row['cdsStart'], row['cdsEnd'])
-        set_gene(db, index, row)
-        #START, STOP
-        close_to(db, row['#name'], row['chrom'], row['cdsStart'], row['cdsEnd'], position_gene, before_after_gene, donor_dict)
+        chr = row['chrom'].replace('chr', '')
+        print(chr, row['txStart'], row['txEnd'])
+        # Call set_gene
+        set_gene(db, index, row, chr)
+        # Call close_to
+        sparseMatrix_before, sparseMatrix_after, gene_name_bef, gene_name_aft = close_to(db, row['#name'], chr, row['txStart'], row['txEnd'], position_out_gene, position_in_gene, before_after_gene, donor_dict, donor_list, gene_name_bef, gene_name_aft, sparseMatrix_before, sparseMatrix_after)
+    # Close file
     before_after_gene.close()
-        
-
-    # # Add to database
-    # db.mydb_connection.commit()
+    write_sparse_matrix(sparseMatrix_before, gene_name_bef, donor_list, save_path, 'bef')
+    # write_sparse_matrix(sparseMatrix_after, gene_name_aft, donor_list, save_path, 'aft')
+    
 
 def get_donors(db):
+    """
+    """
     # DONOR
     db.cursor.execute("""SELECT *
                             FROM donor""")
     donors = db.cursor.fetchall()
+    # Make dictionary with as ket ID and as value donor_ID
     donor_dict = dict()
+    donor_set = set()
     for donor in donors:
+        # Add to donor_dict
         donor_dict[donor['ID']] = donor['donor_ID']
-    return donor_dict
+        donor_set.add(donor['donor_ID'])
+    return donor_dict, list(donor_set)
 
 def main():
     # Path of the database
@@ -188,15 +189,39 @@ def main():
     db = Database(path_db)
     # Path of the genes and there positions
     gene_path = "D:/Hanze_Groningen/STAGE/db/snp132_ucsc_hg19_checkGene.bed" #snp132_ucsc_hg19_checkGene - kopie.bed , snp132_ucsc_hg19_checkGene.bed
+    # Save path
+    save_path = "D:/Hanze_Groningen/STAGE/db/"
     # Read gene file
     gene_df = pd.read_csv(gene_path, sep='\t')
-    position_gene = 2000
-    # print('add value')
+    print(len(gene_df))
+    gene_df['proteinID'].replace('', np.nan, inplace=True)
+    gene_df.dropna(subset=['proteinID'], inplace=True)
+    print(len(gene_df))
+    gene_name_list = gene_df['#name'].tolist()
+    # Positions before and after gene
+    """
+    Hoe Okosun et al. non-coding mutations in promotors aan gen/transcript aanwees: 
+    * Regions of  -2000bp - 250bp (5' UTRs if applicable) from the transcription starting sites (TSS) 
+    for each transcript were screened. For transcripts from the same gene that share the same promoter 
+    mutation profiles, only one representative transcript was selected.
+    """
+    position_out_gene = 2000
+    position_in_gene = 250
+    # Call add_value
     # add_value(db)
     print('set GENE')
-    donor_dict = get_donors(db)
-    loop_over_DOT(db, gene_df, position_gene, donor_dict)
+    # Call get_donors
+    donor_dict, donor_list = get_donors(db)
+    # Creating a len(donor_list) * len(gene_name_list) sparse matrix
+    sparseMatrix_before = csr_matrix((len(donor_list), len(gene_name_list)), 
+                            dtype = np.int8).toarray()
+    sparseMatrix_after = csr_matrix((len(donor_list), len(gene_name_list)), 
+                            dtype = np.int8).toarray()
+    print('loop over genes')
+    # Call loop_over_genes
+    loop_over_genes(db, gene_df, position_out_gene, position_in_gene, donor_dict, donor_list, save_path, gene_name_list, gene_name_list.copy(), sparseMatrix_before, sparseMatrix_after)
     print('CLOSE')
+    # Close database connection
     db.close()
     
 
