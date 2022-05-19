90ea068e27c4be31604380a5a0b94ae0a61c210e
Tijs van Lieshout t.van.lieshout@umcg.nl <you@example.com>
2022-05-19 14:16:15 +0200

    added reading in chunks for creating negative control set of eqtlgen

--- Tijs/preprocess_eqtl_txt2bed.py
+++ Tijs/preprocess_eqtl_txt2bed.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """
 This script can be used to convert the .txt based eQTL data from eQTLGen to a valid BED format.
+TODO: use same formatting as other scripts for meta data
+
+./preprocess_eqtl_txt2bed.py -i ../../GREEN_DB/2019-12-11-cis-eQTLsFDR-Probe
+Level-CohortInfoRemoved-BonferroniAdded.txt.gz -o ../../GREEN_DB/2022-05-13_cis-eqtl_above_p_0_9.bed -m -l -c
 """
 __author__ = "Tijs van Lieshout"
 # Imports
@@ -8,14 +12,23 @@ import argparse
 
 from pybedtools import BedTool as bt
 import pandas as pd
+from tqdm import tqdm
 
 def main(args):
-  convert_txt2bed(args.inputPath, args.outputPath, args.outputMinimal, args.sortLexographically)
+  convert_txt2bed(args.inputPath, args.outputPath, args.outputMinimal, 
+                  args.sortLexographically, args.controlSNPs)
 
 
-def convert_txt2bed(input_path, output_path, output_minimal, sort_lexo):
+def convert_txt2bed(input_path, output_path, output_minimal, sort_lexo, isControlSet):
   if input_path.endswith(".gz"):
-    input_df = pd.read_csv(input_path, sep="\t", compression='gzip')
+    if isControlSet:
+      chunk_size = 10 ** 6
+      input_df = pd.DataFrame()
+      for chunk in tqdm(pd.read_csv(input_path, sep="\t", compression='gzip', chunksize=chunk_size)):
+        processed_chunk = get_control_snps(chunk)
+        input_df = pd.concat([input_df, processed_chunk])
+    else:
+      input_df = pd.read_csv(input_path, sep="\t", compression='gzip')
   else:
     input_df = pd.read_csv(input_path, sep="\t")
 
@@ -34,6 +47,7 @@ def convert_txt2bed(input_path, output_path, output_minimal, sort_lexo):
       minimal_df = minimal_df.sort_values(by=['chrom', 'chromStart']) # sorting is required for many bedtools steps
       minimal_df['chrom'] = 'chr'+minimal_df['chrom'].astype(str)
 
+    minimal_df = minimal_df.drop_duplicates()
     input_bt = bt.from_dataframe(minimal_df.rename(columns={'chrom': '#chrom'}), header=True)
 
   else:
@@ -41,6 +55,11 @@ def convert_txt2bed(input_path, output_path, output_minimal, sort_lexo):
     print("immediate, and thus rough, conversion")
 
   input_bt.saveas(output_path)
+
+
+def get_control_snps(chunk):
+  chunk = chunk[chunk['Pvalue'] == 1]
+  return chunk
   
 
 if __name__ == '__main__':
@@ -50,6 +69,8 @@ if __name__ == '__main__':
   parser.add_argument("-m", "--outputMinimal", action="store_true", required=False, help="Only keep chrom, start, stop and name")
   parser.add_argument("-l", "--sortLexographically", action="store_true", required=False, 
     help="Sort BED file lexographically, such that chr10 comes before chr2. This is the default behaviour for BedTools")
+  parser.add_argument("-c", "--controlSNPs", action="store_true", required=False, 
+    help="Specify if input are control SNPs and you want to only keep p-values near 1")
   args = parser.parse_args()
   
   main(args)
